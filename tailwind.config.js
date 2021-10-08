8c76a41592c40eec3c0a832fcb23d545c68cbf10
Jyrone Parker <jyrone.parker@mgmail.com>
2021-10-08 04:38:30 -0400

    styling add fade of logo on start

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
   theme: {
       extend: {
         animation: {
-         'fade-slow': 'pulse 5s 1',
+         'fade-slow': 'myFade 4s linear 1'
+       },
+       keyframes: {
+         myFade: {
+           '0%': {
+             opacity: '0'
+           },
+           '50%': {
+             opacity: '0.5'
+           },
+           '100%': {
+             opacity: '1'
+           }
+         }
         }
       }
     },
