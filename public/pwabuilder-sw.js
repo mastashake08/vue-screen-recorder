3e48acc1f10e70c217f83ff25dad3494c971584e
Hacker God <jyrone.parker@gmail.com>
2023-09-06 03:20:04 -0400

    4.2.2

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -1,6 +1,6 @@
 // This is the "Offline copy of assets" service worker
 
-const CACHE = "recorder-offline-v4.2.1";
+const CACHE = "recorder-offline-v4.2.2";
 const QUEUE_NAME = "bgSyncQueue";
 const urlsToCache = [
   '/img/logo.5c33d4ac.svg',
@@ -14,14 +14,10 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 async function fetchLatestStats (event) {
   try {
     await fetch(`${API_URL}/get-stats`)
-  } catch (e) {
-    console.log(e)
-  }
+  } catch {}
 }
 
 self.addEventListener('fetch', function(event) {
- console.log(event.request.url);
-
  event.respondWith(
    caches.match(event.request).then(function(response) {
      return response || fetch(event.request);
@@ -35,10 +31,8 @@ self.addEventListener("message", (event) => {
   }
 });
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
   e.waitUntil((async () => {
     const cache = await caches.open(CACHE);
-    console.log('[Service Worker] Caching all: app shell and content');
     await cache.addAll(urlsToCache);
   })());
 });
@@ -61,22 +55,16 @@ workbox.routing.registerRoute(
 self.addEventListener('notificationclick', function(event) {
   if (!event.action) {
     // Was a normal notification click
-    console.log('Notification Click.');
     return;
   }
 
   switch (event.action) {
     case 'donate':
-      console.log('Donate');
       var url = 'https://cash.me/$mastashake08'
       clients.openWindow(url)
       break;
     case 'close':
-      console.log('Close');
       event.notification.close()
-      break;
-    default:
-      console.log(`Unknown action clicked: '${event.action}'`);
       break;
   }
 });
